a4995d36654573de219ac0345966fce81924a60f
endriu <baro.andria@gmail.com>
2026-01-10 05:03:07 +0400

    WIP: test bridge between debug Chrome Instance and Selenium

--- bot.py
+++ bot.py
@@ -1,21 +1,34 @@
-from tkinter.font import names
-
-import pyautogui
+import pyautogui as pag
+import time as t
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
-driver = webdriver.Firefox()
+# for now manually run chrome instance in debug mode:
+# .\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\selenium\chrome-profile"
+# after some time I will automate this from python or from bat
 
-driver.get("https://monkeytype.com/")
+
+chrome_options = Options()
+chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+driver = webdriver.Chrome(options=chrome_options)
+
+
+#driver = webdriver.Chrome()
+# driver.get("https://monkeytype.com/") # commented out for now because it refreshes the page
 # driver.get("http://www.python.org")
-# if driver.title == "Python":
+# t.sleep(2) # needs to get sleep after refresh
+# heading = driver.find_element(By.TAG_NAME, "h1")
+word_container = driver.find_element(By.ID, "words")
+word_elements = word_container.find_elements(By.CLASS_NAME, "word")
 
-heading = driver.find_element(By.TAG_NAME, "h1")
+for w in word_elements:
+    print(w.text)
 
-print(heading.text)
-
+# print(heading.text)
+# print(list(words))
 
 # assert "Python" in driver.title
 #     if driver.title == "Python":
