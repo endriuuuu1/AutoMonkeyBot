2186b8f27c6843963473549eb7c98615734f1706
endriu <baro.andria@gmail.com>
2026-01-12 05:10:09 +0400

    complete Phase 1 (setup) logic + add test configuration class

--- bot.py
+++ bot.py
@@ -2,32 +2,46 @@ import pyautogui as pag
 import time as t
 import socket
 import subprocess
+from configClass import TestConfig
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+
+PORT: int = 9222
+MONKEYTYPE_URL: str = "https://monkeytype.com/"
 
 # manual debug mode for chrome in Terminal
 # C:\Program Files\Google\Chrome\Application\chrome.exe = Dir Address
 # .\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\selenium\chrome-profile"
 
 # automation for chrome debug mode
-def is_port_open(port: int)-> bool:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("127.0.0.1", port)) == 0
+# def is_port_open(port: int)-> bool:
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         return s.connect_ex(("127.0.0.1", port)) == 0
 
-# check if debug mode port is open or not and launch driver instance if it's not
-if not is_port_open(9222):
-    subprocess.Popen([
-        r"C:\Program Files\Google\Chrome\Application\chrome.exe",
-        "--remote-debugging-port=9222",
-        r"--user-data-dir=C:\selenium\chrome-profile"
-    ])
+def session_init():
+    # automation for chrome debug mode
+    def is_port_open(port: int) -> bool:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(("127.0.0.1", port)) == 0
 
-def setup_init():
-    # debug mode browser logic + initial website setup
+    # check if debug mode port is open or not and launch driver instance with asserted port if it's not
+    if not is_port_open(PORT):
+        subprocess.Popen([
+            r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+            f"--remote-debugging-port={PORT}",
+            r"--user-data-dir=C:\selenium\chrome-profile"
+        ])
+
+
+
+
+# all the credential handling logic and automation goes in here - used later for logins
+def auth():
     pass
 
+# for now my account. for future creds.txt/json to login
 def monkey_login():
     # login with my account on monkeytype.com website
     pass
@@ -37,25 +51,33 @@ def remove_blur():
     pass
 
 def restart_test():
-    # tab + enter keyboard input logic
-    pass
+    # tab + enter keyboard input logic: which resets test canvas
+    with pag.hold('tab'):
+        pag.press('enter')
 
-# debug mode options and webdriver assignment
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 # driver.maximize_window() # resizes window to the maximum available screen space
 # driver.fullscreen_window() # invokes fullscreen operation (like pressing F11)
+# driver.get("https://monkeytype.com/")
+# t.sleep(2) # needs to sleep after refresh (driver.get method)
+# word_container = driver.find_element(By.ID, "words")
+# word_elements = word_container.find_elements(By.CLASS_NAME, "word")
+#
+# for w in word_elements:
+#     print(f"word: {w.text.lower()} + index: {word_elements.index(w)}")
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://monkeytype.com/")
 
-t.sleep(2) # needs to sleep after refresh (driver.get method)
-word_container = driver.find_element(By.ID, "words")
-word_elements = word_container.find_elements(By.CLASS_NAME, "word")
+if __name__ == "__main__":
+    # Phase 1: Environment & Session Initialization
+    # Port Check and driver instance launch
+    session_init()
 
-for w in word_elements:
-    print(f"word: {w.text.lower()} + index: {word_elements.index(w)}")
-    # if word_elements.index(w) == 70:
-    #     print("Safety break incoming...")
-    #     break
-driver.close()
+    # Driver Handshake: debug mode options and webdriver assignment
+    chrome_options = Options()
+    chrome_options.add_experimental_option("debuggerAddress", f"127.0.0.1:{PORT}")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Navigation Check: if the current url is monkeytype or not
+    if driver.current_url != MONKEYTYPE_URL:
+        driver.get(MONKEYTYPE_URL)
+    else:
+        pass
---
+++ configClass.py
@@ -0,0 +1,64 @@
+
+
+class TestConfig:
+
+    test_type_list: list = ['time', 'words', 'custom']
+    duration_list: list = [15, 30, 60, 120]
+    language_list: list = ['english',
+                           'english 1k',
+                           'english 5k',
+                           'english 10k',
+                           'english 25k',
+                           'english 450k']
+
+
+    def __init__(self, _type:  str, _duration: int, _language: str, _wpm: int, _punctuation_toggle: bool =None, _numbers_toggle: bool =None)-> None:
+
+        if _type not in Test.test_type_list:
+            raise ValueError(f'Invalid test type. Choose from: {Test.test_type_list}')
+        elif _duration not in Test.duration_list:
+            raise ValueError(f'Invalid duration. Choose from: {Test.duration_list}')
+        elif _language not in Test.language_list:
+            raise ValueError(f'Invalid language. Choose from: {Test.language_list}')
+        elif _wpm not in range(0,351):
+            raise ValueError('Invalid WPM. Choose from between 0 and 350')
+        elif _punctuation_toggle not in [True, False, None]:
+            raise ValueError('Invalid Punctuation Toggle. Choose from [True, False]')
+        elif _numbers_toggle not in [True, False, None]:
+            raise ValueError('Invalid Numbers Toggle. Choose from [True, False]')
+        else:
+            self._wpm = _wpm # speed of test completion (words per minute)
+            self._type = _type # test type
+            self._duration = _duration # test duration
+            self._language = _language # test language
+            self._punctuation_toggle = _punctuation_toggle # test with punctuation in text (boolean)
+            self._numbers_toggle = _numbers_toggle # test with numbers in text
+
+
+    def __str__(self):
+        output = (
+            f'Type of test: {self._type}\n'
+            f'Duration: {self._duration}\n'
+            f'Language: {self._language}\n'
+        )
+
+        if self._punctuation_toggle is True:
+            output += f'Punctuation in text is enabled\n'
+        elif self._punctuation_toggle is False:
+            output += f'Punctuation in text is disabled\n'
+
+        if self._numbers_toggle is True:
+            output += f'Numbers in text enabled\n'
+        elif self._numbers_toggle is False:
+            output += f'Numbers in text is disabled\n'
+
+        return output
+
+
+if __name__ == "__main__":
+
+    obj = TestConfig('words', 30,'english 1k',150)
+    obj2 = TestConfig('time', 30, 'english 5k', 323,True,True)
+
+    print(obj)
+    print(obj2)
